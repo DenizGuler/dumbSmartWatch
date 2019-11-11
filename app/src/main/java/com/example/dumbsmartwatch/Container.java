9d51f89a5b35144e7f0b4852d9326efa20f9da87
Valeriy Soltan <valeriy.soltan@gmail.com>
2019-11-11 17:21:31 -0500

    added permissions, need actual phone to test bluetooth

--- app/src/main/java/com/example/dumbsmartwatch/Container.java
+++ app/src/main/java/com/example/dumbsmartwatch/Container.java
@@ -2,13 +2,19 @@ package com.example.dumbsmartwatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Container extends AppCompatActivity {
+
+    final String watchAddress = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
     }
 }

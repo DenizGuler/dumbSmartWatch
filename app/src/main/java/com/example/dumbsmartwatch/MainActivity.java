44034e90a05c1f338fad0584d87be63e5928ab18
Deniz <dguler@umass.edu>
2019-11-29 19:55:52 -0500

    Updated Main Activity to send you to the Device Scan Activity

--- app/src/main/java/com/example/dumbsmartwatch/MainActivity.java
+++ app/src/main/java/com/example/dumbsmartwatch/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.dumbsmartwatch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startDeviceScanButton;
+
+    BluetoothDevice device;
+    BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+    BluetoothLeScanner scanner = adapter.getBluetoothLeScanner();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startDeviceScanButton = findViewById(R.id.SetUp);
+        startDeviceScanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDeviceScan();
+            }
+        });
+
+
+    }
+
+    private void openDeviceScan() {
+        startActivity(new Intent(this, DeviceScanActivity.class));
     }
 }

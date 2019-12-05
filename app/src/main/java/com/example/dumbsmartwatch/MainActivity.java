2d1ee9a6610b0ea7802446a368ebd21deb7e771f
Deniz <dguler@umass.edu>
2019-12-04 23:43:12 -0500

    MainActivity now opens the notification channel

--- app/src/main/java/com/example/dumbsmartwatch/MainActivity.java
+++ app/src/main/java/com/example/dumbsmartwatch/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
@@ -16,6 +18,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.nfc.Tag;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -38,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        createNotificationChannel();
+
         startDeviceScanButton = findViewById(R.id.SetUp);
         startDeviceScanButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,8 +50,22 @@ public class MainActivity extends AppCompatActivity {
                 openDeviceScan();
             }
         });
+    }
 
-
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Dumb Smart Watch";
+            String description = "channel for dumb smart watch";
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel channel = new NotificationChannel("290M", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
     private void openDeviceScan() {
